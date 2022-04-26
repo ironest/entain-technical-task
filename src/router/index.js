99287e267c03300475a255a37be4de26851a9064
Riccardo Carzaniga <carzaniga.riccardo@gmail.com>
2022-04-27 02:26:20 +1000

    Bare minimum routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import RacesView from '../views/RacesView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import RacesView from '../views/RacesView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: "races",
-      component: RacesView
+      name: 'races',
+      component: RacesView,
+    },
+    {
+      path: '/:catchAll(.*)',
+      redirect: '/',
     }
   ]
 })
 
-export default router
+export default router;
