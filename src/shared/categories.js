e2ead7d18dba9f5a1f0d8a224be424faba855682
Riccardo Carzaniga <carzaniga.riccardo@gmail.com>
2022-04-27 02:25:53 +1000

    created ad-hoc mapping for categories

---
+++ src/shared/categories.js
@@ -0,0 +1,7 @@
+const categories = [
+  { name: 'greyhound', id: '9daef0d7-bf3c-4f50-921d-8e818c60fe61' },
+  { name: 'harness', id: '161d9be2-e909-4326-8c2c-35ed71fb460b' },
+  { name: 'horse', id: '4a2788f8-e825-4d36-9894-efd4baf1cfae' }
+];
+
+export default categories;
