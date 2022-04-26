93d3be29f980bf8e78ea8c1469a66c4d397cda0e
Riccardo Carzaniga <carzaniga.riccardo@gmail.com>
2022-04-27 02:26:34 +1000

    Added filter component

---
+++ src/components/RaceFilters.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="categories">
+    <label v-for="cat in allCategories" :key="cat.id">
+      <input type="checkbox" :value="cat.id" v-model="selectedCategories" @change="handleCheck($event)" />
+      <span>{{ cat.name }}</span>
+    </label>
+  </div>
+</template>
+
+<script>
+import categories from '../shared/categories';
+export default {
+  name: 'RaceFilters',
+  data() {
+    return {
+      allCategories: categories,
+      selectedCategories: [],
+    };
+  },
+  methods: {
+    handleCheck() {
+      this.$emit('filterBy', this.selectedCategories);
+    }
+  },
+  mounted() {
+    this.selectedCategories = this.allCategories.map((c) => c.id);
+    this.$emit('filterBy', this.selectedCategories);
+  },
+};
+</script>
+
+<style></style>
