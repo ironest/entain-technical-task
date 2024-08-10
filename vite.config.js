37912825c0f632d18b0771ee5cfc7e378413e8de
Ricky Carzaniga <ironest@gmail.com>
2024-08-10 20:52:11 +1000

    Adjusted business rules

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,12 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     include: ['tests/**/*.spec.js'],
+    coverage: {
+      provider: 'v8', // Use V8 for coverage
+      reporter: ['text', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{js,vue}'], // Files to include in coverage analysis
+      exclude: ['src/main.js', 'src/App.vue', 'src/router/index.js'], // No testable logic
+    },
   },
 });
